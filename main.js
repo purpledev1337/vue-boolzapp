dd7cc7e76f5c4565ca35a9b5fd87c7003f68a89a
purpledev1337 <88691096+purpledev1337@users.noreply.github.com>
2021-11-16 11:42:38 +0100

    css details+msg print with selectActive function

--- main.js
+++ main.js
@@ -1,13 +1,12 @@
-var carousel = new Vue({
+var app = new Vue({
     el: "#background_main",
     data: {
-        activeContact: 0,
         contacts: [
             {
-            name: 'Michele',
-            avatar: 'img/avatar_1.jpg',
-            visible: true,
-            messages: [
+                name: 'Michele',
+                avatar: 'img/avatar_1.jpg',
+                visible: true,
+                messages: [
                     {
                     date: '10/01/2020 15:30:55',
                     text: 'Hai portato a spasso il cane?',
@@ -26,66 +25,77 @@ var carousel = new Vue({
                 ]
             },
             {
-            name: 'Fabio',
-            avatar: 'img/avatar_2.jpg',
-            visible: true,
-            messages: [
-                    {
-                    date: '20/03/2020 16:30:00',
-                    text: 'Ciao come stai?',
-                    status: 'sent'
-                    },
-                    {
-                    date: '20/03/2020 16:30:55',
-                    text: 'Bene grazie! Stasera ci vediamo?',
-                    status: 'received'
-                    },
-                    {
-                    date: '20/03/2020 16:35:00',
-                    text: 'Mi piacerebbe ma devo andare a fare la spesa.',
-                    status: 'sent'
-                    }
+                name: 'Fabio',
+                avatar: 'img/avatar_2.jpg',
+                visible: false,
+                messages: [
+                        {
+                        date: '20/03/2020 16:30:00',
+                        text: 'Ciao come stai?',
+                        status: 'sent'
+                        },
+                        {
+                        date: '20/03/2020 16:30:55',
+                        text: 'Bene grazie! Stasera ci vediamo?',
+                        status: 'received'
+                        },
+                        {
+                        date: '20/03/2020 16:35:00',
+                        text: 'Mi piacerebbe ma devo andare a fare la spesa.',
+                        status: 'sent'
+                        }
                 ]
             },
             {
-            name: 'Samuele',
-            avatar: 'img/avatar_3.jpg',
-            visible: true,
-            messages: [
-                    {
-                    date: '28/03/2020 10:10:40',
-                    text: 'La Marianna va in campagna',
-                    status: 'received'
-                    },
-                    {
-                    date: '28/03/2020 10:20:10',
-                    text: 'Sicuro di non aver sbagliato chat?',
-                    status: 'sent'
-                    },
-                    {
-                    date: '28/03/2020 16:15:22',
-                    text: 'Ah scusa!',
-                    status: 'received'
-                    }
+                name: 'Samuele',
+                avatar: 'img/avatar_3.jpg',
+                visible: false,
+                messages: [
+                        {
+                        date: '28/03/2020 10:10:40',
+                        text: 'La Marianna va in campagna',
+                        status: 'received'
+                        },
+                        {
+                        date: '28/03/2020 10:20:10',
+                        text: 'Sicuro di non aver sbagliato chat?',
+                        status: 'sent'
+                        },
+                        {
+                        date: '28/03/2020 16:15:22',
+                        text: 'Ah scusa!',
+                        status: 'received'
+                        }
                 ]
             },
             {
-            name: 'Luisa',
-            avatar: 'img/avatar_4.jpg',
-            visible: true,
-            messages: [
-                    {
-                    date: '10/01/2020 15:30:55',
-                    text: 'Lo sai che ha aperto una nuova pizzeria?',
-                    status: 'sent'
-                    },
-                    {
-                    date: '10/01/2020 15:50:00',
-                    text: 'Si, ma preferirei andare al cinema',
-                    status: 'received'
-                    }
+                name: 'Luisa',
+                avatar: 'img/avatar_4.jpg',
+                visible: false,
+                messages: [
+                        {
+                        date: '10/01/2020 15:30:55',
+                        text: 'Lo sai che ha aperto una nuova pizzeria?',
+                        status: 'sent'
+                        },
+                        {
+                        date: '10/01/2020 15:50:00',
+                        text: 'Si, ma preferirei andare al cinema',
+                        status: 'received'
+                        }
                 ]
             },
         ]
+    },
+    methods: {
+        selectActive(index) {
+            this.contacts.find((active)=>{
+                active.visible = false
+            })
+            if (this.contacts[index].visible === false) {
+                this.contacts[index].visible = true;
+                console.log(this.contacts[index].visible);
+            }
+        }
     }
 })
